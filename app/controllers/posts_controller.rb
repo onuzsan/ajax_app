b44cbabfd13a9e7e98c1a05f54b77a03425ffb0b
小沼智則 <omatomemail0718@gmail.com>
2021-03-29 18:11:31 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def create
    
     post = Post.create(content: params[:content])
-    rrender json:{ post: post }
+    render json:{ post: post }
   end
 
 end
